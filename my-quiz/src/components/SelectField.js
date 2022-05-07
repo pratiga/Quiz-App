db45fb08ca6087bfd79c00fdcd87c28f0ea2b6f8
Pratima <gaitrisanu2468@gmail.com>
2022-05-07 17:30:39 +0545

    selection of item
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- my-quiz/src/components/SelectField.js
+++ my-quiz/src/components/SelectField.js
@@ -1,12 +1,32 @@
 import { FormControl, Select, InputLabel, MenuItem } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { handleCategoryChange } from "../redux/actions";
+import { handleDifficultyChange } from "../redux/actions";
+import { handleTypeChange } from "../redux/actions";
 
 const SelectField = (props) => {
     const { label,options } = props;
+    const dispatch = useDispatch()
     const {value, setValue} = useState("");
-    
-    const handleChange = () => {};
+     
+    const handleChange = (e) => {
+        setValue(e.target.value);
+        switch (label) {
+            case "Category":
+            dispatch(handleCategoryChange(e.target.value));
+            break;
+            case "Difficulty":
+            dispatch(handleDifficultyChange(e.target.value));
+            break;
+            case "Category":
+            dispatch(handleTypeChange(e.target.value));
+            break;
+            default:
+            return;
+        }
+    };
     
     return (
         <Box mt={3} width="100%">
