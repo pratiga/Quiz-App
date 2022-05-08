5e77298ee9fbb510ac93704b5e1c5354440b3fd5
Pratima <gaitrisanu2468@gmail.com>
2022-05-08 21:36:07 +0545

    add Questions
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- my-quiz/src/pages/Question.js
+++ my-quiz/src/pages/Question.js
@@ -1,22 +1,69 @@
 import React from 'react'
-import { Button,Typography} from "@mui/material"
+import { Button,CircularProgress,Typography} from "@mui/material"
 import { Box } from "@mui/system";
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import useAxios from "../hooks/useAxions";
 
-const Question = () => {
-  let apiUrl = `/api.php?amount= 10`;
+const getRandomInt = (max) => {
+  return Math.floor(Math.floor(max));
+}
+
+const Questions = () => {
+  const{ 
+        question_category,
+        question_difficulty,
+        question_type,
+        amount_of_question
+        } = useSelector(state => state);
+  let apiUrl = `/api.php?amount=${amount_of_question}`;
+ if(question_category) {
+   apiUrl = apiUrl.concat(`&category = ${question_category}`)
+ }
+ if(question_difficulty) {
+   apiUrl = apiUrl.concat(`&difficulty= ${question_difficulty}`)
+ }
+ if(question_type) {
+   apiUrl = apiUrl.concat(`&type = ${question_type}`)
+ }
+  
   const {response,loading} = useAxios({url:apiUrl })
-  console.log(response)
+  const [questionIndex, setQuestionIndex] = useState(0)
+  const [options,setOptions] = useState([]);
+  console.log(options)
+  
+  useEffect(() => {
+    if(response?.results.lenght) {
+      const question = response.results[questionIndex];
+      let answers = [...question.incorrect_answers];
+      answers.splice(
+        getRandomInt(question.incorrect_answers.length),
+        0,
+        question.incorrect_answers
+      );
+      setOptions(answers);
+    }
+  }, [response, questionIndex])
+  
+  
+  if(loading) {
+    return (
+      <Box mt={20}>
+      <CircularProgress/>
+      </Box>
+    )
+  }
+  
   return (
     <Box>
-    <Typography variant="h4">Question 1</Typography>
-    <Typography mt={5}>this is the question?</Typography>
-    <Box mt={2}>
-      <Button variant="contained">Answer 1</Button>
-    </Box>
-    <Box mt={5}>
-    <Button variant="contained">Answer 2</Button>
-    </Box>
+    <Typography variant="h4">Questions {questionIndex + 1}</Typography>
+    <Typography mt={5}>{response.results[questionIndex].question}</Typography>
+    {options.map((data, id) => (
+      <Box mt={2} >
+      console.log(data)
+        <Button variant='contained'>{data}</Button>
+      </Box>
+    ))}
     <Box mt={5}>
       Score: 2/6
     </Box>
@@ -24,4 +71,4 @@ const Question = () => {
   )
 }
 
-export default Question
+export default Questions
